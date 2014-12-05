5dc53e19fe936c3e111b6bf681ae5bfa3aab33f0
BILLYHO <billyho0906@gmail.com>
2014-12-05 20:38:42 +0800

    Refactor

--- simulate.js
+++ simulate.js
@@ -12,7 +12,7 @@ var del;
 var flagb,flagw,flagf;
 var timer;
 
-clearContent();
+initContent();
 
 function showBlock(name, block)
 {
@@ -20,7 +20,7 @@ function showBlock(name, block)
     var sum = 0;
     for(var i=0; i < block[0].length; i++)
     {
-        r = block[0][i] / 2;
+        var r = block[0][i] / 2;
         context.fillStyle = block[1][i] == 0 ? 'yellow' : 'red';
         context.fillRect(sum, 0, r, 100);
         context.strokeStyle = 'black';
@@ -43,7 +43,8 @@ function add(val)
     flagb = 0;
     var min = 2500;
     var index = -1;
-    for(var i=0; i<blockb[0].length; i++)
+    var i;
+    for(i=0; i<blockb[0].length; i++)
     {
         if(blockb[0][i] >= val && blockb[0][i] <= min && blockb[1][i] == 0) {
             min = blockb[0][i];
@@ -65,7 +66,7 @@ function add(val)
     flagw = 0;
     var max = 0;
     index = -1;
-    for(var i=0; i<blockw[0].length; i++)
+    for(i=0; i<blockw[0].length; i++)
     {
         if (blockw[0][i] >= val && blockw[0][i] > max && blockw[1][i] == 0) {
             max = blockw[0][i];
@@ -84,7 +85,7 @@ function add(val)
     }
 
     flagf = 0;
-    for(var i=0; i<blockf[0].length; i++)
+    for(i=0; i<blockf[0].length; i++)
     {
         if(blockf[0][i] >= val && blockf[1][i]==0)
         {
@@ -108,21 +109,6 @@ function add(val)
     if(messageArr.length > 0)
         alert(messageArr.join(" , ") + (messageArr.length > 1 ? " have " : " has ") +
                     "no more block for this process. Please kick 'clear' and restart.");
-    //if(flagb==0 && flagw==0 && flagf==0)
-    //    alert("Best fit & Worst fit & First fit have no more block for this process. Please kick 'clear' to restart.");
-    //else if(flagb==0 && flagw==0)
-    //    alert("Best fit & Worst fit have no more block for this process. Please kick 'clear' to restart.");
-    //else if(flagw==0 && flagf==0)
-    //    alert("Worst fit & First fit have no more block for this process. Please kick 'clear' to restart.");
-    //else if(flagb==0 && flagf==0)
-    //    alert("Best fit & First fit have no more block for this process. Please kick 'clear' to restart.");
-    //else if(flagb==0 )
-    //    alert("Best fit has no more block for this process. Please kick 'clear' to restart.");
-    //else if(flagw==0 )
-    //    alert("Worst fit has no more block for this process. Please kick 'clear' to restart.");
-    //else if(flagf==0 )
-    //    alert("First fit has no more block for this process. Please kick 'clear' to restart.");
-
 }
 
 
@@ -199,11 +185,11 @@ function stepOver()
     mmsg();
 }
 
-function clearContent()
+function initContent()
 {
-    blockb=[[2500],[0]];
-    blockw=[[2500],[0]];
-    blockf=[[2500],[0]];
+    blockb = [[2500],[0]];
+    blockw = [[2500],[0]];
+    blockf = [[2500],[0]];
 
     nextId = 1;
     processIdArr = [];
@@ -221,32 +207,11 @@ function clearContent()
 
 function mmsg()
 {
-    var mycontext7=document.getElementById("main").getContext('2d');
-    mycontext7.fillStyle='white';
-    mycontext7.fillRect(0,0,500,200);
-    mycontext7.strokeStyle='black';
-    mycontext7.strokeRect(0,0,500,200);
-    mycontext7.fillStyle='black';
-    mycontext7.font = '15pt Calibri';
+    var mycontext7=document.getElementById("main");
     if(flag >=2)
-        mycontext7.fillText( 'Next: Delete ID' + del , 100, 100);
+        mycontext7.innerHTML = 'Next: Delete ID' + del ;
     else
-        mycontext7.fillText( 'Next: Add ' + next + 'KB', 100, 100);
-
-    var mycontext8=document.getElementById("test").getContext('2d');
-    mycontext8.fillStyle='white';
-    mycontext8.fillRect(0,0, 1250, 100);
-    mycontext8.fillStyle='black';
-    mycontext8.font = '15pt Calibri';
-//        mycontext8.fillText(processIdArr, 50, 30);
-//        mycontext8.fillText(nextId, 50, 70);
-//        mycontext8.fillText(flag, 150, 70);
-    mycontext8.fillText(blockb[0], 150, 30);
-    mycontext8.fillText(blockb[1], 50, 30);
-//        mycontext8.fillText(blockw[0].length, 250, 30);
-    mycontext8.fillText(blockw[1], 50, 50);
-//        mycontext8.fillText(blockf[0].length, 450, 30);
-    mycontext8.fillText(blockf[1], 50, 70);
+        mycontext7.innerHTML = 'Next: Add ' + next + 'KB';
 }
 
 function startStimulation()
@@ -260,3 +225,21 @@ function stopStimulation()
 {
     clearTimeout(timer);
 }
+
+//function debugLog()
+//{
+//    var mycontext8=document.getElementById("test").getContext('2d');
+//    mycontext8.fillStyle='white';
+//    mycontext8.fillRect(0,0, 1250, 100);
+//    mycontext8.fillStyle='black';
+//    mycontext8.font = '15pt Calibri';
+//    mycontext8.fillText(processIdArr, 50, 30);
+//    mycontext8.fillText(nextId, 50, 70);
+//    mycontext8.fillText(flag, 150, 70);
+//    mycontext8.fillText(blockb[0], 150, 30);
+//    mycontext8.fillText(blockb[1], 50, 30);
+//    mycontext8.fillText(blockw[0].length, 250, 30);
+//    mycontext8.fillText(blockw[1], 50, 50);
+//    mycontext8.fillText(blockf[0].length, 450, 30);
+//    mycontext8.fillText(blockf[1], 50, 70);
+//}
